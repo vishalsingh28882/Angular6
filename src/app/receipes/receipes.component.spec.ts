d85603c8018c3dc4695d1f381a5f89545725b172
Vishal Singh <vishal.singh28882@gmail.com>
2018-10-07 01:16:33 -0500

    Initial Commit

---
+++ src/app/receipes/receipes.component.spec.ts
@@ -0,0 +1,25 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {ReceipesComponent} from './receipes.component';
+
+describe('ReceipesComponent', () => {
+  let component: ReceipesComponent;
+  let fixture: ComponentFixture<ReceipesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ReceipesComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReceipesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/receipes/receipes.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-receipes',
+  templateUrl: './receipes.component.html',
+  styleUrls: ['./receipes.component.css']
+})
+export class ReceipesComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
