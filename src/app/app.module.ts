6661c9045becc4fc25fa2ee15b33f14703ca34c9
Vishal Singh <vishal.singh28882@gmail.com>
2018-10-07 01:13:40 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ReceipesComponent} from './receipes/receipes.component';
+import {IngredientsComponent} from './ingredients/ingredients.component';
+import {TablesComponent} from './tables/tables.component';
+import {OrdersComponent} from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ReceipesComponent,
+    IngredientsComponent,
+    TablesComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule
