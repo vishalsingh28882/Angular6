cabf21b87c6dc1fc7b1888b53a9b9265956b4276
Vishal Singh <vishal.singh28882@gmail.com>
2018-10-07 19:37:59 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {OrderComponent} from './order/order.component';
+import {TableComponent} from './table/table.component';
+import {IngredientsComponent} from './ingredients/ingredients.component';
+import {ReceipesComponent} from './receipes/receipes.component';
+import {ReceipeComponent} from './receipes/receipe/receipe.component';
+import {IngredientComponent} from './ingredients/ingredient/ingredient.component';
+import {SidenavComponent} from './sidenav/sidenav.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrderComponent,
+    TableComponent,
+    IngredientsComponent,
+    ReceipesComponent,
+    ReceipeComponent,
+    IngredientComponent,
+    SidenavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/receipes/receipe/receipe.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-receipe',
+  templateUrl: './receipe.component.html',
+  styleUrls: ['./receipe.component.css']
+})
+export class ReceipeComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
