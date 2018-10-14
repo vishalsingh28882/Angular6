3de0802635feef96db4ff905ca48cf067bc5f30a
Vishal Singh <vishal.singh28882@gmail.com>
2018-10-13 23:50:38 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HeaderComponent} from './header/header.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 import {MyFormComponent} from './forms/my-form/my-form.component';
+import {MatButtonModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import {MyFormComponent} from './forms/my-form/my-form.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
